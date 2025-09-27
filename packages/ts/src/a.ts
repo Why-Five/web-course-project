cb8e6323f3ade322b4c2bdb2e5172240943380d4
Why-Five <why1605044412@163.com>
2025-09-27 10:57:49 +0800

    const 主=6

---
+++ packages/ts/src/a.ts
@@ -0,0 +1,16 @@
+// src/a.js
+class Person {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    say = (who: string) => {
+        // God：上帝 / god：神
+        const firstChar = who.charAt(0).toLocaleUpperCase();
+        console.log(`${firstChar}${who.slice(1)} bless you`);
+    };
+}
+
+let DPZ = new Person("DPZ");
+let 主 = 'god'; // 6 | 'god' | 'Trisolaran' (三体人)
+DPZ.say(主);
